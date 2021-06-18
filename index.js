46b0d68c2f5fd049170c960eddfbe926e05bfb8e
ronald-luo <r.luo.180@gmail.com>
2021-06-18 19:20:28 -0400

    DFS path-finder

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 let grid = document.querySelector('.node-container')
-for (let i = 0; i < 80*40; i++) {
-    let temp = document.createElement('div')
-    temp.classList.add('node')
-    grid.appendChild(temp)
+
+for (let i = 0; i < 40; i++) {
+    for (let j = 0; j < 40; j++) {
+        let temp = document.createElement('div')
+        temp.classList.add('node')
+        temp.setAttribute('y', i)
+        temp.setAttribute('x', j)
+        grid.appendChild(temp)
+    }
 }
 
 let nodes = document.querySelectorAll('.node')
+
 nodes.forEach((node) => {
     node.addEventListener('click', (e) => {
         node.classList.toggle('applied')
+        console.log(node.getAttribute('x'), node.getAttribute('y'))
     })
 })
+
+// module pattern
+let colorMe = (() => {
+    let i = 0
+    
+    function colorMeExtended () {
+        let nodes = document.querySelectorAll('.node')
+        nodes[i].classList.toggle('applied')
+        i++
+    }
+
+    return {
+        colorMeExtended,
+    }
+})()
+
+setInterval(colorMe.colorMeExtended, 1000)
